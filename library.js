5c9b879087aa885e125a32c4de8a8d9f5f563032
mael <maelsanchez.e@hotmail.com>
2018-12-22 13:25:18 -0600

    first test

--- library.js
+++ library.js
@@ -1,4 +1,12 @@
 "use strict";
+/* globals require, module */
+
+var path = require('path'),
+    fs = require('fs'),
+    mkdirp = require('mkdirp'),
+    mv = require('mv'),
+    async = require('async'),
+    nconf = require.main.require('nconf');
 
 var controllers = require('./lib/controllers'),
 	plugin = {};
@@ -6,14 +14,19 @@ var controllers = require('./lib/controllers'),
 plugin.init = function(params, callback) {
 	var router = params.router,
 		hostMiddleware = params.middleware,
+		multiparty = require.main.require('connect-multiparty')(),
 		hostControllers = params.controllers;
 		
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.	
 	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/thadTest', controllers.renderAdminPage);
+	router.post('/plugins/nodebb-plugin-aps/upload', multiparty, hostMiddleware.validateFiles, hostMiddleware.applyCSRF, controllers.upload);
+
+	// Create "replays/aom" subfolder into upload_path
+	mkdirp(path.join(nconf.get('upload_path'), 'replays/aom'), callback);
 	
-	callback();
+	//callback();
 };
 
 plugin.addAdminNavigation = function(header, callback) {
@@ -24,6 +37,33 @@ plugin.addAdminNavigation = function(header, callback) {
 	});
 
 	callback(null, header);
+};
+
+plugin.processUpload = function(payload, callback) {
+	console.log(payload);
+    /*if (payload.type.startsWith('audio/')) {
+        var id = path.basename(payload.path),
+            uploadPath = path.join(nconf.get('upload_path'), 'audio-embed', id);
+
+        async.waterfall([
+            async.apply(mv, payload.path, uploadPath),
+            async.apply(db.setObject, 'audio-embed:id:' + id, {
+                name: payload.name,
+                size: payload.size
+            }),
+            async.apply(db.sortedSetAdd, 'audio-embed:date', +new Date(), id)
+        ], function(err) {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, {
+                id: id
+            });
+        });
+    } else {
+        callback(new Error('invalid-file-type'));
+    }*/
 };
 
 module.exports = plugin;
--- static/lib/main.js
+++ static/lib/main.js
@@ -1,6 +1,4 @@
 "use strict";
-
-(function() {
 	/*
 		This file shows how client-side javascript can be included via a plugin.
 		If you check `plugin.json`, you'll see that this file is listed under "scripts".
@@ -15,4 +13,63 @@
 
 	console.log('nodebb-plugin-thad-test: Hello world');
 	// Note how this is shown in the console on the first load of every page
-}());
+
+/* globals $, require */
+
+$(document).ready(function() {
+	function upload(callback) {
+		require(['uploader'], function (uploader) {
+			uploader.show({
+				title: 'Upload Audio',
+				description: 'Upload an audio file for embedding into your post',
+				route: config.relative_path + '/plugins/nodebb-plugin-aps/upload'
+			}, callback);
+		});
+	}
+
+	$(window).on('action:composer.loaded', function (ev, data) {
+		require(['composer/formatting', 'composer/controls'], function(formatting, controls) {
+			if (formatting && controls) {
+				formatting.addButtonDispatch('audio-embed', function(textarea, selectionStart, selectionEnd){
+					upload(function (id) {
+						controls.insertIntoTextarea(textarea, '[audio/' + id + ']');
+						controls.updateTextareaSelection(textarea, id.length + 8, id.length + 8);
+					});
+				});
+			}
+		});
+
+		if ($.Redactor) {
+			$.Redactor.opts.plugins.push('audio-embed');
+		}
+	});
+
+	$(window).on('action:redactor.load', function() {
+		$.Redactor.prototype['audio-embed'] = function () {
+			return {
+				init: function () {
+					var self = this;
+
+					// require translator as such because it was undefined without it
+					require(['translator'], function (translator) {
+						translator.translate('Embed Audio', function (translated) {
+							var button = self.button.add('audio-embed', translated);
+							self.button.setIcon(button, '<i class="fa fa-file-audio-o"></i>');
+							self.button.addCallback(button, self['audio-embed'].onClick);
+						});
+					});
+				},
+				onClick: function () {
+					var self = this;
+					upload(function (id) {
+						templates.parse('partials/audio-embed', {
+							path: config.relative_path + '/uploads/audio-embed/' + id
+						}, function (html) {
+							self.insert.html(html);
+						});
+					});
+				}
+			};
+		};
+	});
+});
